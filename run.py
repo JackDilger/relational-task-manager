a2789d2ff7d2ac1601161ff30b888207e26d2c6b
JackDilger <jack.dilger1995@gmail.com>
2022-07-25 07:59:32 +0000

    add

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from taskmanager import app
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT"))
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
     )
   
